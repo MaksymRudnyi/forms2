8b54ef3f15c831f03ddce1ae3097b413f282da75
maxim <maxim@webro.com.ua>
2016-07-21 23:15:20 +0300

    try to find out what is wrong

--- app/hero-form.component.ts
+++ app/hero-form.component.ts
@@ -10,14 +10,18 @@ export class HeroFormComponent {
         'Super Hot', 'Weather Changer'];
     model = new Hero(18, 'Dr IQ', this.powers[0], 'Chuck Overstreet');
     submitted = false;
-    onSubmit() { this.submitted = true; }
+    onSubmit() {
+        this.submitted = true;
+        console.log('this submited', this.submitted);
+    }
     // TODO: Remove this when we're done
     get diagnostic() { return JSON.stringify(this.model); }
 
-    active = true;
+    //active = true;
     newHero() {
-        this.model = new Hero(42, '', '');
-        this.active = false;
-        setTimeout(() => this.active = true, 0);
+        //this.model = new Hero(42, '', '');
+        this.submitted = true;
+        //this.active = false;
+        //setTimeout(() => this.active = true, 0);
     }
 }
